1a3db58ca443143378eb4298c80561039a4c46e5
ycui11 <cuiyih@gmail.com>
2018-04-29 23:55:21 -0500

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,14 +1,3 @@
-#########################################################################################################################
-# Author: Safa Messaoud                                                                                                 #
-# E-Mail: messaou2@illinois.edu                                                                                         #
-# Instituation: University of Illinois at Urbana-Champaign  															#
-# Course: ECE 544_na Fall 2017                                                            								#
-# Date: July 2017                                                                                                   	#
-#                                                                                                                       #
-# Description: 																											#
-#	Script containg the configuration of the Denoising Autoencoder Model 												#
-#																														#
-#########################################################################################################################
 
 
 
